3ab9b56fff1d284bd1734fb5c8b786446079a040
Lucas Santos <lhs.santoss@gmail.com>
2020-01-11 01:19:40 -0300

    :art: ESLint fix

--- src/components/ProfileForm/index.vue
+++ src/components/ProfileForm/index.vue
@@ -9,14 +9,9 @@
       <p class="text-body1 text-grey-6 q-mb-sm">
         Username
       </p>
-      <q-input
-        filled
-        v-model="form.name"
-        label="Seu username"
-        :rules="[
+      <q-input filled v-model="form.name" label="Seu username" :rules="[
           value => validators.notEmpty(value) || 'Este campo é obrigatório'
-        ]"
-      />
+        ]" />
       <p class="text-body1 text-grey-6 q-mb-sm">
         Email
       </p>
@@ -29,31 +24,17 @@
           <p class="text-body1 text-grey-6 q-mb-sm">
             Username
           </p>
-          <q-input
-            ref="username"
-            filled
-            type="text"
-            debounce="500"
-            v-model="form.username"
-            :rules="[
+          <q-input ref="username" filled type="text" debounce="500" v-model="form.username" :rules="[
               value => validators.notEmpty(value) || 'Este campo é obrigatório'
-            ]"
-            label="Username *" class="q-my-xs profile-form-sm-input" />
+            ]" label="Username *" class="q-my-xs profile-form-sm-input" />
         </div>
         <div class="column">
           <p class="text-body1 text-grey-6 q-mb-sm">
             RG
           </p>
-          <q-input
-            ref="document"
-            filled
-            type="text"
-            v-model="form.document"
-            label="Numero do RG *"
-            :rules="[
+          <q-input ref="document" filled type="text" v-model="form.document" label="Numero do RG *" :rules="[
               value => validators.notEmpty(value) || 'Este campo é obrigatório'
-            ]"
-            class="q-my-xs profile-form-sm-input" />
+            ]" class="q-my-xs profile-form-sm-input" />
         </div>
       </div>
 
@@ -65,15 +46,9 @@
       </div>
     </q-card-section>
     <q-card-section class="row justify-end">
-      <q-btn
-        :class="{
+      <q-btn :class="{
           'full-width': $q.platform.is.mobile
-        }"
-        class="text-family-regular"
-        size="lg"
-        color="primary"
-        label="Salvar"
-        @click="emitClick" />
+        }" class="text-family-regular" size="lg" color="primary" label="Salvar" @click="emitClick" />
     </q-card-section>
   </q-card>
 </template>
@@ -85,18 +60,22 @@ import { notEmpty } from '../../utils/validators';
 export default {
   name: 'ProfileForm',
   components: { RegionSelect },
-  data: () => ({
-    validators: { notEmpty },
-    form: {
-      name: 'Igor Halfeld',
-      email: 'hello@igorluiz.me',
-      username: 'igorhalfeld',
-      document: '999999999',
-  methods: {
-    emitClick() {
-      this.$emit('profile-submit', this.form);
-    },
-    },
+  data() {
+    return {
+      validators: { notEmpty },
+      form: {
+        name: 'Igor Halfeld',
+        email: 'hello@igorluiz.me',
+        username: 'igorhalfeld',
+        document: '999999999',
+      },
+      methods: {
+        emitClick() {
+          this.$emit('profile-submit', this.form);
+        },
+      },
+    };
+  },
 };
 </script>
 
