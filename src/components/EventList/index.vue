d8d6dbf71debebcde419126039b20765d7175454
italo jose <italo.i@live.com>
2020-03-23 22:55:03 -0300

    componented eventList

---
+++ src/components/EventList/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="row justify-center col-md-4 col-12 q-my-md q-ma-md q-px-md">
+        <event-card
+          v-for="(event, index) in events"
+          :aspect-ratio="cardAspectRatio.LANDSCAPE"
+          :key="`event-${index}`"
+          class="q-my-sm"
+          :event="event"
+           @click="$emit('detail-event', event)"
+        />
+    </div>
+</template>
+<script>
+import EventCard, { aspectRatios } from '../EventCard'
+
+export default {
+  name: 'EventList',
+  components: { EventCard },
+  mounted: () => {
+  },
+  data: () => {
+    return {
+      cardAspectRatio: aspectRatios
+    }
+  },
+  props: {
+    events: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
--- src/pages/Dashboard/General.vue
+++ src/pages/Dashboard/General.vue
@@ -1,8 +1,7 @@
 <template>
   <!-- <div class="fit column items-center"> -->
   <div class="fit">
-    <!-- backgroundImage: 'url(https://secure.meetupstatic.com/photos/event/c/3/3/e/highres_486769982.jpeg)',
-        backgroundPosition: '50% 50%', -->
+
     <banner
       component="EventList"
       title="Netwoking. Conhecimento. Felicidade."
@@ -12,17 +11,6 @@
       }"
       sub-title="Junte-se a um grupo e faça mais do que você ama."
       additional-info="" />
-    <!--
-    <banner
-      component="NextMeetup"
-      title="NodeBR #59 - Masters of Node.js"
-      :styles="{
-        /* backgroundImage: 'linear-gradient(45deg, #e6549e, #027BE3)' */
-        backgroundColor: '#18104a'
-      }"
-      sub-title="NodeBR São Paulo"
-      additional-info="" />
-    -->
 
     <div class="row justify-center full-width q-mt-xl">
       <div class="col-md-6 col-12 q-mx-md q-px-md">
@@ -38,51 +26,30 @@
       </div>
     </div>
 
-    <div class="row justify-center full-width">
-      <div class="row justify-center col-md-4 col-12 q-my-md q-ma-md q-px-md">
-        <event-card
-          v-for="(event, index) in events"
-          :aspect-ratio="cardAspectRatios.LANDSCAPE"
-          :key="`event-${index}`"
-          class="q-my-sm"
-          :event="event"
+    <div class="row justify-center full-width q-mt-xl">
+        <event-list
+            :events=  "events"
         />
-      </div>
         <group-list
             :groups=  "groups"
             @detail-group="goToGroup"
         />
     </div>
 
-    <!-- <div class="row justify-center full-width">
-      <div class="row justify-center col-md-6 col-12 q-my-md q-ma-md q-px-md">
-        <group-card
-          v-for="(group, index) in groups"
-          :aspect-ratio="cardAspectRatios.LANDSCAPE"
-          :key="`event-${index}`"
-          class="q-my-sm"
-          :group="group"
-           @click="goToGroup"
-        />
-      </div>
-    </div> -->
-
   </div>
 </template>
 
 <script>
 import Banner from '../../components/Banner'
-import EventCard, { aspectRatios } from '../../components/EventCard'
-// import GroupCard from '../../components/GroupCard'
 import GroupList from '../../components/GroupList'
+import EventList from '../../components/EventList'
 
 export default {
   name: 'HomePage',
-  components: { Banner, EventCard, GroupList },
+  components: { Banner, GroupList, EventList },
   data: () => ({
     events: [],
     groups: [],
-    cardAspectRatios: aspectRatios,
     search: ''
   }),
   mounted () {
